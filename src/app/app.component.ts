1742c4d808102928d551338429bd61e6da56c472
jbnjs149 <jbnjs149@gmail.com>
2021-12-27 23:58:49 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,20 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Empty Validation using Custom Directive';
   errorMessage: string;
   userNotes: string = '';
 
   submit(submitForm: NgForm) {
     console.log('Form Valid: ', submitForm.valid);
-    console.log('notes', )
-    if(!submitForm.valid) {
-      this.errorMessage = "please enter notes";
+    console.log('notes');
+    if (!submitForm.valid) {
+      this.errorMessage = 'please enter notes';
+    } else {
+      this.errorMessage = '';
     }
   }
 }
